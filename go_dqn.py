066de5f341eb83d057f7fe0743627dc036737154
DableUTeeF <palm22180@gmail.com>
2018-05-28 22:23:09 +0700

    Fixed resign bugs

--- go_dqn.py
+++ go_dqn.py
@@ -10,7 +10,7 @@ from keras.layers import Dense, Conv2D, Flatten, BatchNormalization, Activation,
 from keras.optimizers import Adam, SGD
 import pachi_py
 K.set_image_data_format('channels_first')
-from copy import deepcopy
+import sys
 
 # todo: 1). Point out where is legal move on explore stage
 # todo: 2). Should I use single Conv2D with large kernel
@@ -24,9 +24,9 @@ class DQNAgent:
         self.state_size = state_size
         self.action_size = action_size
         self.memory = deque(maxlen=1000)
-        self.gamma = 0.95  # discount rate
-        self.epsilon = 1.  # exploration rate
-        self.epsilon_min = 0.15
+        self.gamma = 0.8  # discount rate
+        self.epsilon = 0.9  # exploration rate
+        self.epsilon_min = 0.25
         self.epsilon_decay = 0.995
         self.model = self._build_model()
         self.iteration = 0
@@ -35,26 +35,43 @@ class DQNAgent:
     def _build_model(self):
         # Neural Net for Deep-Q learning Model
         model = Sequential()
-        model.add(Conv2D(64, 3, input_shape=self.state_size, padding='same'))  # input dimension = #states
+        model.add(Conv2D(32, 3, input_shape=self.state_size, padding='same'))  # input dimension = #states
         model.add(BatchNormalization())
         model.add(Activation('relu'))
         model.add(Conv2D(64, 3, padding='same'))  # input dimension = #states
         model.add(BatchNormalization())
         model.add(Activation('relu'))
-        model.add(Conv2D(64, 3, padding='same'))  # input dimension = #states
+        model.add(Conv2D(64, 1, padding='same'))  # input dimension = #states
         model.add(BatchNormalization())
         model.add(Activation('relu'))
-        model.add(Conv2D(64, 3, padding='same'))  # input dimension = #states
+        model.add(Conv2D(128, 3, padding='same'))  # input dimension = #states
         model.add(BatchNormalization())
         model.add(Activation('relu'))
-        model.add(Conv2D(64, 3, padding='same'))  # input dimension = #states
+        model.add(Conv2D(128, 1, padding='same'))  # input dimension = #states
         model.add(BatchNormalization())
         model.add(Activation('relu'))
-        model.add(Conv2D(64, 1, activation='relu', padding='same'))  # output nodes = #action
+        model.add(Conv2D(256, 3, padding='same'))  # input dimension = #states
+        model.add(BatchNormalization())
+        model.add(Activation('relu'))
+        model.add(Conv2D(256, 1, padding='same'))  # input dimension = #states
+        model.add(BatchNormalization())
+        model.add(Activation('relu'))
+        model.add(Conv2D(512, 3, padding='same'))  # input dimension = #states
+        model.add(BatchNormalization())
+        model.add(Activation('relu'))
+        model.add(Conv2D(512, 1, padding='same'))  # input dimension = #states
+        model.add(BatchNormalization())
+        model.add(Activation('relu'))
+        model.add(Conv2D(64, 1, padding='same'))  # output nodes = #action
+        model.add(BatchNormalization())
+        model.add(Activation('relu'))
         model.add(Reshape((self.action_size*64,)))
-        model.add(Dense(self.action_size+1, activation='sigmoid'))
-        model.compile(loss='binary_crossentropy',
-                      optimizer=SGD(lr=1e-2, decay=1e-5, momentum=0.9))
+        model.add(Dense(self.action_size+1, activation='softmax'))
+        model.compile(loss='categorical_crossentropy',
+                      optimizer=Adam(lr=1e-4, decay=1e-6))
+        # model.add(Dense(self.action_size+1, activation='sigmoid'))
+        # model.compile(loss='binary_crossentropy',
+        #               optimizer=Adam(lr=1e-2, decay=1e-6))
 
         print(model.summary())
         return model
@@ -65,20 +82,17 @@ class DQNAgent:
         i = 0
         board_size = int(self.action_size ** 0.5)
         if np.random.rand() <= self.epsilon:
-            if np.random.rand() <= 1:
-                while True:
-                    target = random.randrange(self.action_size)
-                    x, y = int(target/board_size), (target-(int(target/board_size)*board_size))
-                    if state[0][2][x][y] == 1:
-                        if not (self.f == 0 and x == y == int(board_size/2)):
-                            self.f = 1
-                            return target
-                    elif np.sum(state[0][2]) == 0:
-                        return self.action_size
+            for i in range(self.action_size):
+                target = random.randrange(self.action_size-1)
+                x, y = int(target/board_size), (target-(int(target/board_size)*board_size))
+                if state[0][2][x][y] == 1:
+                    if not (self.f == 0 and x == y == int(board_size/2)):
+                        self.f = 1
+                        return target
+                elif np.sum(state[0][2]) == 0:
+                    return self.action_size
 
-                    i += 1
-            else:
-                return random.randrange(1, self.action_size)
+                i += 1
         act_values = self.model.predict(state)
 
         for i in range(self.action_size+1):
@@ -86,13 +100,12 @@ class DQNAgent:
             target = srt[-1*i]
             x, y = int(target / board_size), (target - (int(target / board_size) * board_size))
             if target == self.action_size:
-                return self.action_size
+                continue
             if state[0][2][x][y] == 1:
-                if not (self.f == 0 and x == y == int(board_size / 2)):
+                if not (self.f == 0 and x == y == int(board_size / 2)) and target != self.action_size:
                     self.f = 1
                     return target
-            elif np.sum(state[0][2]) == 0:
-                return self.action_size
+        return self.action_size
 
     def remember(self, state, action, reward, next_state, done):  # done==True if this is the ending move
         self.memory.append((state, action, reward, next_state, done))
@@ -113,12 +126,12 @@ class DQNAgent:
 
 
 if __name__ == "__main__":
-    board_size = 7
+    board_size = 11
     bb = Board(board_size)
     bb._turn = bb.BLACK
     env = GoEnv(player_color='black',
-                # opponent='pachi:uct:_2400',
-                opponent='random',
+                opponent='pachi:uct:_2400',
+                # opponent='random',
                 observation_type='image3c',
                 illegal_move_mode='lose',
                 board_size=board_size)
@@ -126,26 +139,36 @@ if __name__ == "__main__":
     action_size = board_size**2
     print("{} actions, {}-dim state".format(action_size, state_size))
     agent = DQNAgent(state_size, action_size)
+    try:
+        agent.model.load_weights('old-{}.h5'.format(board_size))
+    except:
+        pass
     batch_size = 32
     illegal = 0
     e = 0
+    win = 0
+    lose = 0
+    draw = 0
     while True:
         e += 1
-
-        if e == 100000:
-            env.dl_model = agent.model
-            env.dl_model.load_weights('old.h5')
-            env.opponent = 'dl'
-        elif e > 100000:
-            env.dl_model.load_weights('old.h5')
+        if e == 500000:
+            env.opponent = 'pachi:uct:_2400'
+        # if e == 10:
+        #     env.dl_model = agent.model
+            # env.dl_model.load_weights('old-{}.h5'.format(board_size))
+        #     env.opponent = 'dl'
+        # elif e > 10:
+        #     env.dl_model = agent.model
+            # env.dl_model.load_weights('old-{}.h5'.format(board_size))
 
         state = env.reset()
         agent.f = 0
         # state = np.rollaxis(state, 0, 3)
         state = np.array([state])
         success = False
-        # i = 0
+        i = 0
         while True:
+            i += 1
             # env.render()
             # bb._turn = bb.BLACK
             # for row in range(board_size):
@@ -173,25 +196,24 @@ if __name__ == "__main__":
             next_state = np.array([next_state])
             agent.remember(state, action, reward, next_state, done)
             state = next_state
-            if done:
+            if reward == -1:
+                reward = 0
+            if reward == 1:
+                win += reward
+            if done or i > 5000:
+                if reward == 0:
+                    lose += 1
                 bb = Board(board_size)
-                print("episode: {}, action: ({},{}), e: {:.2}, illegal: {}"
-                      .format(e+1,
+                print("episode: {}, action: ({},{}), win: {}/{}, e: {:.2}, illegal: {}"
+                      .format(e,
                               board_size - int(action/board_size),
                               chr(action-(int(action/board_size)*board_size)+1+64),
+                              int(win), int(lose),
                               agent.epsilon,
-                              isillegal))
+                              illegal))
                 env.render()
                 break
 
         if len(agent.memory) > batch_size:
-            agent.model.save_weights('old.h5')
+            agent.model.save_weights('old-{}.h5'.format(board_size))
             agent.replay(batch_size)
-
-        if success:
-            success_count += 1
-        else:
-            success_count = 0
-        if success_count > 100:
-            print('SOLVED!!!')
-            break
